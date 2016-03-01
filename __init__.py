081ea6ee88303eb25536c34ae4b62dd2627b2052
Alexey-T <support@uvviewsoft.com>
2016-03-02 02:59:00 +0300

    uses new spell lib

--- __init__.py
+++ __init__.py
@@ -1,20 +1,17 @@
+import os
+import sys
+import string
 from cudatext import *
 from bisect import bisect_left
-import string
-import os
+sys.path.append(os.path.dirname(__file__))
+import enchant
+
 
 COLOR_UNDER = 0xFF #red underline
 BORDER_UNDER = 6 #wave underline
-MARKTAG = 106 #uniq int for all marker plugins
+MARKTAG = 105 #uniq int for all marker plugins
 
-dict_filename = os.path.join(os.path.dirname(__file__), 'dict', 'en_US.dic')
-dict_words = []
-
-def do_read_dict():
-    global dict_words
-    text = open(dict_filename, encoding='cp1251').read().splitlines()
-    dict_words = sorted([s.split('/')[0].lower() for s in text])
-    #print('dict:', dict_words[:5])
+dict_obj = enchant.Dict('en_US')
 
 def is_word_char(s):
     chars = string.ascii_letters+string.digits+"'_"
@@ -26,16 +23,8 @@ def is_word_alpha(s):
     if s[0] in "'_": return False
     return True    
 
-def is_word_in_list(x, a):
-    hi = len(a)
-    pos = bisect_left(a, x, 0, hi)
-    res = (pos != hi) and (a[pos] == x)
-    #print('word', x, 'res:', res)
-    return res
-
 
 def do_hilite():
-    global dict_words
     ed.attr(MARKERS_DELETE_BY_TAG, MARKTAG)
     COLOR_FORE = ed.get_prop(PROP_COLOR, 'EdTextFont')
     COLOR_BACK = ed.get_prop(PROP_COLOR, 'EdTextBg')
@@ -57,17 +46,12 @@ def do_hilite():
             sub = s[n1:n2]
             n1 = n2
             if not is_word_alpha(sub): continue
-            if is_word_in_list(sub.lower(), dict_words): continue
+            if dict_obj.check(sub): continue
             
             ed.attr(MARKERS_ADD, MARKTAG, text_x, text_y, len(sub),   
               COLOR_FORE, COLOR_BACK, COLOR_UNDER, 
               0, 0, 0, 0, 0, BORDER_UNDER)
-            #print('word', sub)
 
 class Command:
-    def __init__(self):
-        do_read_dict()
-    
-    def run(self):
+    def hilite(self):
         do_hilite()
-        
