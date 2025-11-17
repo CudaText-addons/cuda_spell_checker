ba313337419d09d5271cd8773d9059f5d78a05b8
Alexey T. <support@uvviewsoft.com>
2025-11-17 09:53:55 +0300

    small optim

--- __init__.py
+++ __init__.py
@@ -724,7 +724,7 @@ def do_work_word(ed, with_dialog):
 def get_next_pos(x1, y1, is_next):
     m = ed.attr(MARKERS_GET)
     if not m: return
-    m = [(x, y) for (tag, x, y, nlen, c1, c2, c3, f1, f2, f3, b1, b2, b3, b4, som, mo) in m if tag == MARKTAG]
+    m = [(x, y) for (tag, x, y, *_) in m if tag == MARKTAG]
     if not m: return
 
     if is_next:
